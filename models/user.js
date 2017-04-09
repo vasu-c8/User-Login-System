8db5fb4d087da5e3e65b16b527b36eab73aec4fd
Vasundhara <vasudha.c8@gmail.com>
2017-04-08 22:01:27 -0700

    Login authentication with passport

--- models/user.js
+++ models/user.js
@@ -26,6 +26,21 @@ var userSchema = mongoose.Schema({
 
 var User = module.exports=mongoose.model('User',userSchema);
 
+module.exports.getUserById = function(id,callback) {
+	User.findById(id,callback);
+}
+
+module.exports.getUserByUsername = function(username,callback) {
+	var query =  {username: username};
+	User.findOne(query,callback);
+}
+
+module.exports.comparePassword = function(candidatePassword, hash, callback){
+	bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+    	callback(null, isMatch);
+	});
+}
+
 module.exports.createUser = function(newUser,callback){
 
     bcrypt.genSalt(10, function(err, salt) {
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 var multer = require('multer');
 var upload = multer({dest: './uploads'});
+var passport = require('passport');
+
+var localStrategy = require('passport-local').Strategy;
 
 var User= require('../models/user');
 
@@ -17,6 +20,45 @@ router.get('/register', function(req, res, next) {
 router.get('/login', function(req, res, next) {
   res.render('login',{title:'Login'});
 });
+
+router.post('/login',
+  passport.authenticate('local',{failureRedirect:'/users/login', failureFlash: 'Invalid Username or Password'}),
+  function(req, res) {
+    req.flash('success','You are now logged in');
+
+    res.redirect('/');
+
+  });
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.getUserById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new localStrategy(function(username,password,done) {
+   User.getUserByUsername(username,function(err,user){
+       if(err) throw err;
+       if(!user){
+        return done(null,false,{message: 'Unknown User'})
+       }
+
+    User.comparePassword(password,user.password,function(err,isMatch) {
+      if(err) return done(err);
+      if(isMatch){
+        return done(null,user);
+      }
+      else{
+        return done(null,false,{message: 'Invalid Password'});
+      }
+    });   
+   });
+  
+}));
 
 router.post('/register', upload.single('profileimage'),function(req, res, next) {
   var name=req.body.name;
